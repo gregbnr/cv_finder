c7d7f2d6b7db6a03f3d5277c7f2cd5c0b5805e38
gregoirebonnier <gbonnier@optimetriks.com>
2022-03-18 17:59:45 +0100

    update gitignore

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const apiKey = 'TODO';
-  static const projectBase = 'TODO';
+  static const apiKey = 'keykSp50X8GfqdsjR';
+  static const projectBase = 'appBJsfqvid7OaKfw';
   static const recordName = 'CvFinder';
 }
